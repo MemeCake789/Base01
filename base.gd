688b5b6f66b8079034997f6b985b17540f69bef9
Mem3 <memecake788@gmail.com>
2024-10-29 20:11:50 -0500

    Basic Base Concepts
    + Spawn items around base (enemies)

---
+++ base.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var enemy_spawn_pivot: Marker2D = %EnemySpawnPoint
+
+# Properties
+@export var radius : int = 100
+ 
+var item_scene = load("res://temp.tscn") 
+
+func _ready():
+	var spawn_point_position = enemy_spawn_pivot.global_position
+
+	# Spawn enemies randomly around the spawn point
+	for _i in range(10):
+		# Generate a random angle between 0 and -π radians
+		var angle = randf_range(0, -PI)
+
+		# Calculate x and y coordinates
+		var x = cos(angle) * radius
+		var y = sin(angle) * radius
+
+		var start_position = spawn_point_position + Vector2(x, y)
+
+		var item = item_scene.instantiate() 
+		item.position = start_position
+		add_child(item)
