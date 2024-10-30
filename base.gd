30e22d8d0bc5a90ded8b9d05ea297167029077c5
Mem3 <memecake788@gmail.com>
2024-10-30 08:10:40 -0500

    Spawn Fix
    ~ Fixed spawn issue in main.tscn

--- base.gd
+++ base.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var enemy_spawn_pivot: Marker2D = %EnemySpawnPoint
 
 # Properties
-@export var radius : int = 100
+@export var radius : int = 500
  
 var item_scene = load("res://temp.tscn") 
 
@@ -13,13 +13,13 @@ func _ready():
 	# Spawn enemies randomly around the spawn point
 	for _i in range(10):
 		# Generate a random angle between 0 and -π radians
-		var angle = randf_range(0, -PI)
+		var angle = randf_range(-0.34, -PI + 0.34)
 
 		# Calculate x and y coordinates
 		var x = cos(angle) * radius
 		var y = sin(angle) * radius
 
-		var start_position = spawn_point_position + Vector2(x, y)
+		var start_position = Vector2(x, y)
 
 		var item = item_scene.instantiate() 
 		item.position = start_position
