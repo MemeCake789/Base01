c28fb54e4e91c9887123326570156886fa9755d2
Mem3 <memecake788@gmail.com>
2024-11-01 11:27:54 -0500

    Camera Zoom and Crosshair
    + Camera Zoom on Right Click
    + Added crosshair

--- player.gd
+++ player.gd
@@ -1,29 +1,34 @@
 extends CharacterBody2D
 
-var speed = 400  			# Maximum speed in pixels/sec
-var acceleration = 1000  	# How quickly to reach max speed
-var friction = 500  		# How quickly to slow down
+const ZOOM_IN_TARGET = Vector2(2.0, 2.0)  # Target zoom level
+const ZOOM_OUT_TARGET = Vector2(1.0, 1.0)  # Initial zoom level
+const ZOOM_SPEED = 10.0      # Zoom speed factor
 
+var speed = 400# Maximum speed in pixels/sec
+var acceleration = 1000# How quickly to reach max speed
+var friction = 500# How quickly to slow down
 
-@onready var player: CharacterBody2D = %Player
-@export var enemy_scene : PackedScene
-# Properties
-@export var radius : int = 500
- 
+var is_zooming = false
+var target_zoom = ZOOM_OUT_TARGET
 
 func _ready():
 	pass
 
-
-func _physics_process(delta)	:
-	# Get input direction vector
+func _physics_process(delta):
+	# Character movement logic
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	
-	# If there's input, accelerate in that direction
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward(direction * speed, acceleration * delta)
-	# If no input, slow down to a stop
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
-	
 	move_and_slide()
+	look_at(get_global_mouse_position())
+	# Zooming logic
+	
+	$Camera2D.zoom = lerp($Camera2D.zoom, target_zoom, ZOOM_SPEED * delta)
+
+func _input(event):
+	if event.is_action_pressed("right_click"):
+		target_zoom = ZOOM_IN_TARGET
+	elif event.is_action_released("right_click"):
+		target_zoom = ZOOM_OUT_TARGET
